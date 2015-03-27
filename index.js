a07e32e1193c0d402faa3847ffad20443c2c2387
Charles Bido <charlesbido@SolarKing.home>
2015-03-26 21:28:00 -0500

    update

--- index.js
+++ index.js
@@ -23,3 +23,7 @@ io.on('connection', function(socket) {
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
 
 app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");  
+
+http.listen(app.get('port'), app.get('ipaddr'), function(){
+  console.log('Express server listening on  IP: ' + app.get('ipaddr') + ' and port ' + app.get('port'));
+});
